03a8c3818815493969249acccd895e44f3541773
homumado <nobisan@gmail.com>
2022-09-30 11:55:43 -1000

    add desc explanation

--- app/routes/notes/$noteId/edit.tsx
+++ app/routes/notes/$noteId/edit.tsx
@@ -1,6 +1,6 @@
 import type { ActionArgs, LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Form, useLoaderData } from "@remix-run/react";
+import { Form, Link, useLoaderData, useLocation } from "@remix-run/react";
 import invariant from "tiny-invariant";
 
 import { getNote, updateNote } from "~/models/note.server";
@@ -19,50 +19,70 @@ export async function loader({ request, params }: LoaderArgs) {
 
 export default function EditNotePage() {
   const data = useLoaderData<typeof loader>();
+  const location = useLocation();
 
   return (
-    <Form
-      method="post"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 8,
-        width: "100%",
-      }}
-    >
-      <div>
-        <label className="flex w-full flex-col gap-1">
-          <span>Title: </span>
-          <input
-            defaultValue={data.note.title}
-            required
-            name="title"
-            className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
-          />
-        </label>
-      </div>
+    <>
+      <Form
+        method="post"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 8,
+          width: "100%",
+        }}
+      >
+        <div>
+          <label className="flex w-full flex-col gap-1">
+            <span>Title: </span>
+            <input
+              defaultValue={data.note.title}
+              required
+              name="title"
+              className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
+            />
+          </label>
+        </div>
 
+        <div>
+          <label className="flex w-full flex-col gap-1">
+            <span>Body: </span>
+            <textarea
+              defaultValue={data.note.body}
+              required
+              name="body"
+              rows={8}
+              className="w-full flex-1 rounded-md border-2 border-blue-500 py-2 px-3 text-lg leading-6"
+            />
+          </label>
+        </div>
+        <div className="text-right">
+          <button
+            type="submit"
+            className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+          >
+            Save
+          </button>
+        </div>
+      </Form>
       <div>
-        <label className="flex w-full flex-col gap-1">
-          <span>Body: </span>
-          <textarea
-            defaultValue={data.note.body}
-            required
-            name="body"
-            rows={8}
-            className="w-full flex-1 rounded-md border-2 border-blue-500 py-2 px-3 text-lg leading-6"
-          />
-        </label>
+        This Form after-submit behavior changes depending how you navigated to
+        it.
+        <ul className="list-inside list-disc">
+          <li>
+            If you access it via an <Link to=".">internal Link</Link>, its
+            inputs will not update from loader after submit.
+          </li>
+          <li>
+            If you access it via a browser refresh (
+            <a href={location.pathname} target="_blank" rel="noreferrer">
+              or url directly
+            </a>
+            ), its inputs will update from loader after submit.
+          </li>
+        </ul>
       </div>
-      <div className="text-right">
-        <button
-          type="submit"
-          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
-        >
-          Save
-        </button>
-      </div>
-    </Form>
+    </>
   );
 }
 
